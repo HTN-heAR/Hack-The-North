73bd8b5dc65a33e5750af613f732d7005fc9db70
Kenny Zhao <kennyzhao2004@gmail.com>
2022-09-17 18:14:14 -0400

    Update app.py

--- nlp/app.py
+++ nlp/app.py
@@ -6,9 +6,14 @@ return: translation
 """
 from flask import Flask, request
 from googletrans import Translator
+import cohere
 
 app = Flask(__name__)
 translator = Translator()
+
+api_key = ""
+co = cohere.Client(api_key)
+
 
 def get_translation(received_data):
     translated_text = translator.translate(received_data)
@@ -16,7 +21,18 @@ def get_translation(received_data):
 
 
 def get_summary():
-    return "summarized paragraph"
+    prompt = '''"" In summary: ""'''
+    #parameters to change outcome of summary
+    co.generate(
+    model='large', #model size 
+    prompt=prompt, #the prompt
+    temperature = 0.1, #number to determine likelyhood of random responses
+    k = 500, #ensures top k of number of tokens to generate
+    p = 1, #t ensures that only the most likely tokens, with total probability mass of p, are considered for generation at each step
+    frequency_penalty= 0.5 #penalizes new tokens based on their existing frequency in the text so far
+    presence_penalty= 0.5 #penalizes new tokens based on whether they appear in the text so far
+    )
+    return prompt
 
 
 @app.route("/")
