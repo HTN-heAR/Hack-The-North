15036b2c569a35a6cd5f6a6595e8d6173fb40344
luwai <luwaiwong@gmail.com>
2022-09-17 23:12:18 -0400

    W

--- Hack The North/Assets/Scripts/FuckYou.cs
+++ Hack The North/Assets/Scripts/FuckYou.cs
@@ -78,8 +78,8 @@ public class FuckYou : MonoBehaviour
             string[] pages = uri.Split('/');
             int page = pages.Length - 1;
 
-            print(webRequest.result);
-            response = webRequest.result.ToString();
+            print(webRequest.downloadHandler.text);
+            response = webRequest.downloadHandler.text;
             translatedDisplay.text = response;
             switch (webRequest.result)
             {
