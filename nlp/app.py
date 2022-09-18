ccb4d2a350f78fd60434e22ad2919a6a1af07c7d
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-09-17 20:12:25 -0400

    Update app.py

--- nlp/app.py
+++ nlp/app.py
@@ -1,9 +1,4 @@
-"""
-send: raw transcription
-
-return: summary
-return: translation
-"""
+import textwrap
 from flask import Flask, request
 from googletrans import Translator
 import cohere
@@ -11,7 +6,7 @@ import cohere
 app = Flask(__name__)
 translator = Translator()
 
-api_key = ""
+api_key = "NWAA0EelG8ZZOYr4XePYJ6l86WUewGZU9UzuxcRd"
 co = cohere.Client(api_key)
 
 
@@ -20,31 +15,38 @@ def get_translation(received_data):
     return translated_text.text
 
 
-def get_summary():
-    prompt = '''"" In summary: ""'''
+def get_summary(received_data):
+    prompt = textwrap.dedent(f'''Passage: {received_data}
+    TLDR: ''')
+
+    print(prompt)
+
     #parameters to change outcome of summary
-    co.generate(
-    model='large', #model size 
-    prompt=prompt, #the prompt
-    temperature = 0.1, #number to determine likelyhood of random responses
-    k = 500, #ensures top k of number of tokens to generate
-    p = 1, #t ensures that only the most likely tokens, with total probability mass of p, are considered for generation at each step
-    frequency_penalty= 0.5, #penalizes new tokens based on their existing frequency in the text so far
-    presence_penalty= 0.5, #penalizes new tokens based on whether they appear in the text so far
+    prediction = co.generate(
+        model='large', #model size
+        prompt=prompt, #the prompt
+        temperature = 0.1, #number to determine likelihood of random responses
+        k = 500, #ensures top k of number of tokens to generate
+        p = 1, #t ensures that only the most likely tokens, with total probability mass of p, are considered for generation at each step
+        frequency_penalty= 0.5, #penalizes new tokens based on their existing frequency in the text so far
+        presence_penalty= 0.5, #penalizes new tokens based on whether they appear in the text so far
     )
-    return prompt
+
+    print(prediction)
+
+    return "tba"
 
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "heAR"
 
 
 # @app.route('/login', methods=['GET'])
 @app.route('/summary')
 def summary():
     res = request.args.get("q")
-    return res
+    return get_summary(res)
 
 @app.route('/translate')
 def translate():
@@ -53,3 +55,10 @@ def translate():
 
 if __name__ == "__main__":
     app.run()
+
+"""
+send: raw transcription
+
+return: summary
+return: translation
+"""
